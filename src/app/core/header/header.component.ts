5c5ba9350478b087e4a50efb1a20976e45460f78
Joao <jhoudoris@gmail.com>
2020-07-07 18:33:27 -0300

    start profile

---
+++ src/app/core/header/header.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+
+  public show: boolean;
+  constructor(public auth: AngularFireAuth, private router: Router) { }
+
+  ngOnInit() { 
+    this.auth.user;
+  }
+
+  logout() {
+    this.auth.auth.signOut();
+    this.router.navigate(["signin"]);
+  }
+
+}
---
+++ src/app/profiles/profiles.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProfileDetailComponent } from './profile-detail/profile-detail.component';
+
+
+
+@NgModule({
+  declarations: [ProfileDetailComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ProfilesModule { }
