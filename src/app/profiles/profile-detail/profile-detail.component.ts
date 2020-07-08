b123ecc8e62bc8594f1443bc076dab0345a4883c
Joao <jhoudoris@gmail.com>
2020-07-08 19:10:47 -0300

    user profile data

--- src/app/profiles/profile-detail/profile-detail.component.ts
+++ src/app/profiles/profile-detail/profile-detail.component.ts
@@ -1,15 +1,77 @@
-import { Component, OnInit } from '@angular/core';
+import { TokenService } from './../../core/shared/token.service';
+import { Component, OnInit, Input, OnDestroy } from '@angular/core';
+
+import { AngularFireAuth } from '@angular/fire/auth';
+import { User } from 'firebase';
 
 @Component({
   selector: 'app-profile-detail',
   templateUrl: './profile-detail.component.html',
   styleUrls: ['./profile-detail.component.css']
 })
-export class ProfileDetailComponent implements OnInit {
+export class ProfileDetailComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  public user: User;
+  public userNick: string;
+
+  public image;
+
+  constructor( private auth: AngularFireAuth, private tokenService: TokenService) { }
+  ngOnDestroy(): void {
+    this.setNickName(this.userNick)
+  }
 
   ngOnInit() {
+    this.getUser();
+  }
+
+  public getUser(): void {
+    this.auth.user.subscribe(user => {
+      this.user = user as User;
+      if (!!this.user.uid) {
+        this.getNickName();
+        this.getUserImage();
+       }
+     });
+  }
+
+  public setNickName(nick: string): void {
+    this.tokenService.setToken(this.user.uid, nick);
+  }
+
+  public getNickName(): void {
+    let nick = this.tokenService.getToken(this.user.uid);
+    this.userNick = nick;
+  }
+
+  public getUserImage():void {
+    let image = this.tokenService.getToken(`image-${this.user.uid}`);
+    this.image = image;
+  }
+
+
+  public removeImage(): void {
+    this.tokenService.removeToken(`image-${this.user.uid}`);
+    this.image = null;
+  }
+
+  public changeListener($event) : void {
+    this.readImage($event.target);
+  }
+
+  public readImage(inputValue: any): void {
+    var file:File = inputValue.files[0];
+    var fileReader:FileReader = new FileReader()  ;
+    if (file.type === 'image/jpeg' || file.type === 'image/png') {
+      fileReader.onloadend = (e) => {
+        this.image = fileReader.result;
+        this.tokenService.setToken(`image-${this.user.uid}` , fileReader.result );
+      }
+      fileReader.readAsDataURL(file);
+    } else {
+      alert("Selecione uma imagem JPG ou PNG")
+      return;
+    }
   }
 
 }
--- src/app/profiles/profiles.module.ts
+++ src/app/profiles/profiles.module.ts
@@ -2,13 +2,14 @@ import { ProfilesRoutingModule } from './profiles.routing.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProfileDetailComponent } from './profile-detail/profile-detail.component';
+import { FormsModule } from '@angular/forms';
 
 
 
 @NgModule({
   declarations: [ProfileDetailComponent],
   imports: [
-    CommonModule, ProfilesRoutingModule
+    CommonModule, ProfilesRoutingModule, FormsModule
   ]
 })
 export class ProfilesModule { }
