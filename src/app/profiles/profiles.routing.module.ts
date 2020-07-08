874397db59d3baef5c3ca7aa3e8bf22bc11f69bf
Joao <jhoudoris@gmail.com>
2020-07-08 19:19:18 -0300

    file

---
+++ src/app/profiles/profiles.routing.module.ts
@@ -0,0 +1,15 @@
+import { ProfileDetailComponent } from './profile-detail/profile-detail.component';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+
+const routes: Routes = [
+  {
+    path: "",
+    component: ProfileDetailComponent,
+  }
+];
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class ProfilesRoutingModule {}
---
+++ src/app/profiles/shared/user.ts
@@ -0,0 +1,9 @@
+
+export class User {
+    public displayName?: string;
+    public email?: string;
+    public phoneNumber?: string;
+    public photoURL?: string;
+    public providerId?: string;
+    public uid: string;
+}
